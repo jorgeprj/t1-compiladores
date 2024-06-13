8416485c19f0a623c9592e4109bc264ff73a1a9b
jorgeprj <jlmp2002@gmail.com>
2024-06-13 11:58:33 -0300

    fix: bugs

--- novo-la-lexico/src/main/java/br/ufscar/dc/compiladores/novo/la/lexico/App.java
+++ novo-la-lexico/src/main/java/br/ufscar/dc/compiladores/novo/la/lexico/App.java
@@ -1,47 +1,42 @@
 package br.ufscar.dc.compiladores.novo.la.lexico;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.Token;
+import java.io.PrintWriter;
 
 public class App {
-
     public static void main(String[] args) {
         String arquivoSaida = args[1];
-        
-        try (PrintWriter pw = new PrintWriter(arquivoSaida)) {
+        try {
             CharStream cs = CharStreams.fromFileName(args[0]);
+
             LaLexer lex = new LaLexer(cs);
-            Token t;
+            Token t = null;
+
+            PrintWriter pw = new PrintWriter(arquivoSaida);
 
             while ((t = lex.nextToken()).getType() != Token.EOF) {
                 String nomeToken = LaLexer.VOCABULARY.getDisplayName(t.getType());
-                String mensagem = null;
-                
-                switch (nomeToken) {
-                    case "SIMBOLO_NAO_ENCONTRADO":
-                        mensagem = "Linha " + t.getLine() + ": " + t.getText() + " - simbolo nao identificado";
-                        break;
-                    case "COMENTARIO_NAO_FECHADO":
-                        mensagem = "Linha " + t.getLine() + ": comentario nao fechado";
-                        break;
-                    case "CADEIA_LITERAL_NAO_FECHADA":
-                        mensagem = "Linha " + t.getLine() + ": cadeia literal nao fechada";
-                        break;
-                    default:
-                        mensagem = "<'" + t.getText() + "'," + nomeToken + ">";
-                        break;
-                }
-
-                pw.println(mensagem);
-                pw.flush();
-
-                if (mensagem.contains("simbolo nao identificado") || mensagem.contains("comentario nao fechado") || mensagem.contains("cadeia literal nao fechada")) {
+                if (nomeToken.equals("SIMBOLO_NAO_ENCONTRADO")) {
+                    pw.println("Linha " + t.getLine() + ": " + t.getText() + " - simbolo nao identificado");
                     break;
+                } else if (nomeToken.equals("COMENTARIO_NAO_FECHADO")) {
+                    pw.println("Linha " + t.getLine() + ": comentario nao fechado");
+                    break;
+                } else if (nomeToken.equals("CADEIA_LITERAL_NAO_FECHADA")) {
+                    pw.println("Linha " + t.getLine() + ": cadeia literal nao fechada");
+                    break;
+                } else {
+                    pw.println("<'" + t.getText() + "'," + nomeToken + ">");
                 }
+
+                pw.flush();
             }
+
+            pw.close();
+
         } catch (IOException ex) {
             ex.printStackTrace();
         }
